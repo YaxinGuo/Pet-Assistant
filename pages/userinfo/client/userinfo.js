125934beeb10516f7305212d84dff1427134d434
YaxinGuo <yxguo0922@brandeis.edu>
2017-06-20 14:18:29 -0400

    remove insecure package

--- pages/userinfo/client/userinfo.js
+++ pages/userinfo/client/userinfo.js
@@ -9,14 +9,32 @@ Template.addpet.events({
     const gender = instance.$('#gender').val();
     const ownername = instance.$('#ownername').val();
     const birthyear = parseInt(year);
+
+    petnameinputs = instance,$("#")
     console.log('adding '+name);
     instance.$('#name').val("");
     instance.$('#birthyear').val("");
     instance.$('#gender').val("");
     instance.$('#ownername').val("");
-    Pet.insert({name:name,birthyear:birthyear,gender:gender,ownername:ownername,
-                owner:Meteor.userId(),
-                creatAt:new Date()});
+    pets = Pet.find({owner:Meteor.userId()}).fetch();
+
+
+    var petname = {name:name,
+                   birthyear:birthyear,
+                   gender:gender,
+                   ownername:ownername,
+                   owner:Meteor.userId(),
+                   creatAt:new Date()}
+    Meteor.call('petname.insert',petname,
+    ((err,res) => {
+      console.log('got the answer');
+      console.dir({err,res});
+      }
+    ));
+
+  //  Pet.insert({name:name,birthyear:birthyear,gender:gender,ownername:ownername,
+          //      owner:Meteor.userId(),
+            //    creatAt:new Date()});
     //People.insert({name.birthyear})
   }
 })
@@ -24,14 +42,17 @@ Template.petrow.helpers({
   isOwner(){console.dir(this);
     return this.pet.owner == Metor.userid()}
 })
+
 Template.petrow.events({
-  'click span'(elt,instance) {
+  'click #erase'(elt,instance){
     console.dir(this);
     console.log(this.pet._id);
-    if (this.pet.owner==Meteor.userId()){
+    Meteor.call('petname.remove',this.pet._id);
+  /*  if (Meteor.userId()==this.person.owner){
        Pet.remove(this.pet._id);
     }else{
       alert("Why are you deleting someone else's entry?");
     }
+    */
   }
 })
